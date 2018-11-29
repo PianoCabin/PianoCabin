f6baa6f613ec6b6fdf6e46190fa04fe200fa10cb
linlinhaohao888 <linlinhaohao.888@163.com>
2018-11-29 12:02:08 +0800

    feedback 页面调整

--- Admin-Frontend/src/module/feedback/App.vue
+++ Admin-Frontend/src/module/feedback/App.vue
@@ -3,37 +3,63 @@
     <heading></heading>
 
     <el-container>
-      <el-aside width="18rem"><side-bar activated="4" class="side-bar"></side-bar></el-aside>
+      <el-aside width="18rem">
+        <side-bar activated="4" class="side-bar"></side-bar>
+      </el-aside>
 
       <el-main>
         <div class="content select-panel">
           <el-tabs v-model="activated" @tab-click="handleTabClick">
             <el-tab-pane label="全部" name="0">
-              <news-card @click="getDetail" class="news-card" v-for="feedback in feedback_list.slice(page_start,page_end)">
-                <nobr slot="title">{{feedback.feedback_title}}</nobr>
-                <nobr slot="publisher">{{feedback.user_id}}</nobr>
-                <nobr slot="publish-time">{{feedback.time|getDate}}&nbsp{{feedback.time|getTime}}</nobr>
-                <nobr slot="status">{{feedback.read_status|read_status}}</nobr>
-                <nobr slot="card-id">{{feedback.feedback_id}}</nobr>
-              </news-card>
+              <el-table :data="feedback_list.slice(page_start,page_end)" style="width: 100%" highlight-current-row
+                        @row-click="getDetail">
+                <el-table-column prop="user_id" label="反馈人" width="300"></el-table-column>
+                <el-table-column prop="feedback_title" label="标题" width="250"></el-table-column>
+                <el-table-column label="内容" width="600">
+                  <template slot-scope="scope"><p class="feedback-content">{{ scope.row.content }}</p></template>
+                </el-table-column>
+                <el-table-column label="反馈时间" width="220">
+                  <template slot-scope="scope"><p>{{ scope.row.feedback_time | getFullTime}}</p></template>
+                </el-table-column>
+                <el-table-column label="阅读情况" width="100">
+                  <template slot-scope="scope"><p>{{ scope.row.read_status | getStatus}}</p></template>
+                </el-table-column>
+                <el-table-column class-name="feedback-id" prop="feedback_id" width="0"></el-table-column>
+              </el-table>
             </el-tab-pane>
             <el-tab-pane label="已读" name="1">
-              <news-card @click="getDetail" class="news-card" v-for="feedback in read_feedback_list.slice(page_start,page_end)">
-                <nobr slot="title">{{feedback.feedback_title}}</nobr>
-                <nobr slot="publisher">{{feedback.user_id}}</nobr>
-                <nobr slot="publish-time">{{feedback.time|getDate}}&nbsp{{feedback.time|getTime}}</nobr>
-                <nobr slot="status">{{feedback.read_status|read_status}}</nobr>
-                <nobr slot="card-id">{{feedback.feedback_id}}</nobr>
-              </news-card>
+              <el-table :data="read_feedback_list.slice(page_start,page_end)" style="width: 100%" highlight-current-row
+                        @row-click="getDetail">
+                <el-table-column prop="user_id" label="反馈人" width="300"></el-table-column>
+                <el-table-column prop="feedback_title" label="标题" width="250"></el-table-column>
+                <el-table-column label="内容" width="600">
+                  <template slot-scope="scope"><p class="feedback-content">{{ scope.row.content }}</p></template>
+                </el-table-column>
+                <el-table-column label="反馈时间" width="220">
+                  <template slot-scope="scope"><p>{{ scope.row.feedback_time | getFullTime}}</p></template>
+                </el-table-column>
+                <el-table-column label="阅读情况" width="100">
+                  <template slot-scope="scope"><p>{{ scope.row.read_status | getStatus}}</p></template>
+                </el-table-column>
+                <el-table-column class-name="feedback-id" prop="feedback_id" width="0"></el-table-column>
+              </el-table>
             </el-tab-pane>
             <el-tab-pane label="未读" name="2">
-              <news-card @click="getDetail" class="news-card" v-for="feedback in unread_feedback_list.slice(page_start,page_end)">
-                <nobr slot="title">{{feedback.feedback_title}}</nobr>
-                <nobr slot="publisher">{{feedback.user_id}}</nobr>
-                <nobr slot="publish-time">{{feedback.time|getDate}}&nbsp{{feedback.time|getTime}}</nobr>
-                <nobr slot="status">{{feedback.read_status|read_status}}</nobr>
-                <nobr slot="card-id">{{feedback.feedback_id}}</nobr>
-              </news-card>
+              <el-table :data="unread_feedback_list.slice(page_start,page_end)" style="width: 100%" highlight-current-row
+                        @row-click="getDetail">
+                <el-table-column prop="user_id" label="反馈人" width="300"></el-table-column>
+                <el-table-column prop="feedback_title" label="标题" width="250"></el-table-column>
+                <el-table-column label="内容" width="600">
+                  <template slot-scope="scope"><p class="feedback-content">{{ scope.row.content }}</p></template>
+                </el-table-column>
+                <el-table-column label="反馈时间" width="220">
+                  <template slot-scope="scope"><p>{{ scope.row.feedback_time | getFullTime}}</p></template>
+                </el-table-column>
+                <el-table-column label="阅读情况" width="100">
+                  <template slot-scope="scope"><p>{{ scope.row.read_status | getStatus}}</p></template>
+                </el-table-column>
+                <el-table-column class-name="feedback-id" prop="feedback_id" width="0"></el-table-column>
+              </el-table>
             </el-tab-pane>
           </el-tabs>
         </div>
@@ -61,47 +87,34 @@
   import * as Vue from "vue";
   import Utils from "../../common/js/utils"
 
-  Vue.filter('read_status',function (int) {
-    if (int === 1)
-      return "已读"
-    else
-      return "未读"
+  // 获取阅读状态文字
+  Vue.filter('getStatus', function (val) {
+    return val ? "已读" : "未读"
   })
 
-  Vue.filter('getDate', function (timestamp) {
-    let time = new Date(parseFloat(timestamp) * 1000)
-    let year = time.getFullYear().toString()
-    let month = (time.getMonth() + 1).toString()
-    let date = time.getDate().toString()
-    return [year, month, date].join('-')
-  })
-
-  // 根据时间戳获取时间
-  Vue.filter('getTime', function (timestamp) {
-    let time = new Date(parseFloat(timestamp) * 1000)
-    let hour = time.getHours().toString()
-    let minute = time.getMinutes().toString()
-    return [hour, minute].join(':')
+  // 获取完整时间
+  Vue.filter('getFullTime', function (timestamp) {
+    return new Date(parseFloat(timestamp) * 1000).toLocaleString()
   })
 
   export default {
     name: "App",
     components: {NewsCard, SideBar, Heading, OrderCard},
-    created(){
-      Utils.get(this,"/a/feedback/list/",null,function (_this,res) {
-        if(res.code === 0){
+    created() {
+      Utils.get(this, "/a/feedback/list/", null, function (_this, res) {
+        if (res.code === 0) {
           _this.$message.error("获取反馈失败")
         }
-        else{
+        else {
           console.log(res)
           _this.feedback_list = res.data.feedback_list
           let len = _this.feedback_list.length
 
           _this.total_len = len
           _this.page_start = 0
-          _this.page_end = Math.min(_this.page_size,len)
+          _this.page_end = Math.min(_this.page_size, len)
 
-          for(let i = 0; i < len ; i++){
+          for (let i = 0; i < len; i++) {
             switch (_this.feedback_list[i].read_status) {
               case 0:
                 _this.unread_feedback_list.push(_this.feedback_list[i]);
@@ -116,7 +129,7 @@
         }
       })
     },
-    data () {
+    data() {
       return {
         activated: "0",
 
@@ -126,7 +139,7 @@
 
         page_start: 0,
 
-        page_end: 0,
+        page_end: 2,
 
         page_size: 10,
 
@@ -139,56 +152,56 @@
     },
 
     methods: {
-      handlePageChange : function (val) {
+      handlePageChange: function (val) {
         // TO DO
         this.page_start = (val - 1) * this.page_size
         switch (this.activated) {
           case "0":
             this.total_len = this.feedback_list.length
-            this.page_end = Math.min(this.page_start + this.page_size,this.feedback_list.length)
+            this.page_end = Math.min(this.page_start + this.page_size, this.feedback_list.length)
             break
           case "1":
             this.total_len = this.read_feedback_list.length
-            this.page_end = Math.min(this.page_start + this.page_size,this.read_feedback_list.length)
+            this.page_end = Math.min(this.page_start + this.page_size, this.read_feedback_list.length)
             break
           case "2":
             this.total_len = this.unread_feedback_list.length
-            this.page_start = Math.min(this.page_start + this.page_size,this.unread_feedback_list.length)
+            this.page_start = Math.min(this.page_start + this.page_size, this.unread_feedback_list.length)
             break
           default:
             break
         }
       },
-      handleTabClick : function (tab,event) {
+      handleTabClick: function (tab, event) {
         this.current_page = 1
         this.page_start = 0
         switch (tab.name) {
           case "0":
             this.total_len = this.feedback_list.length
-            this.page_end = Math.min(this.feedback_list.length,this.page_size)
+            this.page_end = Math.min(this.feedback_list.length, this.page_size)
             break
           case "1":
             this.total_len = this.read_feedback_list.length
-            this.page_end = Math.min(this.read_feedback_list.length,this.page_size)
+            this.page_end = Math.min(this.read_feedback_list.length, this.page_size)
             break
           case "2":
             this.total_len = this.unread_feedback_list.length
-            this.page_end = Math.min(this.unread_feedback_list.length,this.page_size)
+            this.page_end = Math.min(this.unread_feedback_list.length, this.page_size)
             break
           default:
             break
         }
       },
-      getDetail(feedback_id){
-        console.log(feedback_id);
-        Utils.get(this,'/a/feedback/detail?feedback_id='+feedback_id,null,function (_this,res) {
-          if(res.code === 0){
+
+      // 响应单行点击
+      getDetail(row, event, column) {
+        let feedback_id = parseInt(event.currentTarget.getElementsByClassName('feedback-id')[0].innerText)
+        Utils.get(this, '/a/feedback/detail?feedback_id=' + feedback_id, null, function (_this, res) {
+          if (res.code === 0) {
             _this.$message.error("获取反馈详细信息失败")
-          }
-          else{
+          } else {
             console.log(res.data)
           }
-
         })
       }
     }
@@ -211,17 +224,6 @@
     margin: 2rem auto 1rem;
   }
 
-  .fab {
-    width: 5rem;
-    height: 5rem;
-    border-radius: 5rem;
-    font-size: 3rem;
-    box-shadow: 2px 2px 2px #888888;
-    position: fixed;
-    bottom: 4rem;
-    right: 4rem;
-  }
-
   .pagination-div {
     position: relative;
     top: 2rem;
@@ -230,6 +232,12 @@
 
   .pagination {
     width: fit-content !important;
-    margin:0 auto !important;
+    margin: 0 auto !important;
+  }
+
+  .feedback-content {
+    white-space: nowrap !important;
+    overflow: hidden !important;
+    text-overflow: ellipsis !important;
   }
 </style>
