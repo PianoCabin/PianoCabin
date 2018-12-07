503ea1055b7de2c8929c8a10adbf87afff5c3b36
linlinhaohao888 <linlinhaohao.888@163.com>
2018-12-07 10:29:45 +0800

    添加部分管理员端测试

--- Backend/user_end/tests.py
+++ Backend/user_end/tests.py
@@ -5,59 +5,59 @@ import json
 
 
 # Create your tests here.
-# class LoginTest(TestCase):
-#
-#     # 测试Login API
-#     def setUp(self):
-#         self.client = Client()
-#
-#     def test_post(self):
-#         response = self.client.post('/u/login/', {
-#             'code': '0333HhU02scqeZ0wYIX02RIpU023HhUB'
-#         })
-#         self.assertEqual(response.json()['code'], 0)
+class LoginTest(TestCase):
+
+    # 测试Login API
+    def setUp(self):
+        self.client = Client()
+
+    def test_post(self):
+        response = self.client.post('/u/login/', {
+            'code': '0333HhU02scqeZ0wYIX02RIpU023HhUB'
+        })
+        self.assertEqual(response.json()['code'], 0)
 
 
-# class PianoListTest(TestCase):
-#
-#     # 测试PianoList API
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.room_1 = PianoRoom.objects.create(
-#             room_num='F2-203',
-#             piano_type='钢琴房',
-#             brand='星海立式钢琴',
-#             price_0=15,
-#             price_1=10,
-#             price_2=5,
-#             usable=True
-#         )
-#
-#         cls.room_2 = PianoRoom.objects.create(
-#             room_num='F2-205',
-#             piano_type='钢琴房',
-#             brand='星海立式钢琴',
-#             price_0=15,
-#             price_1=10,
-#             price_2=5,
-#             usable=True
-#         )
-#
-#         cls.user = User.objects.create(
-#             open_id='xxxxxxxxxxxxxxxxx',
-#             session='aaaaaaaaaaaaaaaaa'
-#         )
-#
-#         redis_manage.session_user.set(cls.user.session, cls.user.id)
-#
-#     def setUp(self):
-#         self.client = Client()
-#
-#     def test_post(self):
-#         response = self.client.post('/u/order/piano-rooms-list/', {
-#             'date': datetime.now().timestamp(),
-#             'type': '钢琴房',
-#             'authorization': self.user.session
-#         })
-#
-#         print(response.json()['data'])
+class PianoListTest(TestCase):
+
+    # 测试PianoList API
+    @classmethod
+    def setUpTestData(cls):
+        cls.room_1 = PianoRoom.objects.create(
+            room_num='F2-203',
+            piano_type='钢琴房',
+            brand='星海立式钢琴',
+            price_0=15,
+            price_1=10,
+            price_2=5,
+            usable=True
+        )
+
+        cls.room_2 = PianoRoom.objects.create(
+            room_num='F2-205',
+            piano_type='钢琴房',
+            brand='星海立式钢琴',
+            price_0=15,
+            price_1=10,
+            price_2=5,
+            usable=True
+        )
+
+        cls.user = User.objects.create(
+            open_id='xxxxxxxxxxxxxxxxx',
+            session='aaaaaaaaaaaaaaaaa'
+        )
+
+        redis_manage.initDatabase()
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_post(self):
+        response = self.client.post('/u/order/piano-rooms-list/', {
+            'date': datetime.now().timestamp(),
+            'type': '钢琴房',
+            'authorization': self.user.session
+        }, content_type='application/json', HTTP_AUTHORIZATION=self.user.session)
+
+        print(response.json()['data'])
