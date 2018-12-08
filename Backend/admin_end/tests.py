d2b5516c6b907f718b3e7e059a5c261d678e9b2b
liuyijian <liuyijian16@163.com>
2018-12-08 10:42:29 +0800

    test for piano-room

--- Backend/admin_end/tests.py
+++ Backend/admin_end/tests.py
@@ -21,7 +21,8 @@ class MyTest(TestCase):
         cls.user.set_password(cls.password)
         cls.user.save()
         cls.client = Client()
-        cls.login_client = Client().login(username=cls.username, password=cls.password)
+        cls.login_client = Client()
+        cls.login_client.login(username=cls.username, password=cls.password)
         redis_manage.initDatabase()
 
 
@@ -111,7 +112,7 @@ class PianoRoomCreateTest(MyTest):
 class PianoRoomEditTest(MyTest):
     @classmethod
     def setUpTestData(cls):
-        temp_room = PianoRoom.objects.create(
+        cls.temp_room = PianoRoom.objects.create(
             room_num='F2-202',
             piano_type='电钢琴',
             brand='杂牌',
@@ -121,18 +122,121 @@ class PianoRoomEditTest(MyTest):
             usable=1,
             art_ensemble=0
         )
+        cls.piano_room_msg_dict_edit = {
+            'room_num': 'F2-202',
+            'piano_type': '电子琴',
+            'brand': '新牌',
+            'price_0': 100,
+            'price_1': 80,
+            'price_2': 60,
+            'usable': False,
+            'art_ensemble': 1
+        }
 
     def test_post(self):
-        self.login_client
-
+        response = self.login_client.post('/a/piano-room/edit/', self.piano_room_msg_dict_edit)
+        self.assertEqual(response.json()['code'], 1)
 
 
 class PianoRoomDeleteTest(MyTest):
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.temp_room = PianoRoom.objects.create(
+            room_num='F2-202',
+            piano_type='电钢琴',
+            brand='杂牌',
+            price_0=10,
+            price_1=8,
+            price_2=6,
+            usable=1,
+            art_ensemble=0
+        )
+        cls.piano_room_msg_dict_delete_correct = {
+            'room_num': 'F2-202',
+        }
+        cls.piano_room_msg_dict_delete_fault = {
+            'room_num': 'F2-201',
+        }
+
+    def test_post(self):
+        response = self.login_client.post('/a/piano-room/delete/', self.piano_room_msg_dict_delete_correct)
+        self.assertEqual(response.json()['code'], 1)
+        response = self.login_client.post('/a/piano-room/delete/', self.piano_room_msg_dict_delete_fault)
+        self.assertEqual(response.json()['code'], 0)
 
 
 class PianoRoomListTest(MyTest):
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        # piano_type room_num 可组合选择
+        cls.temp_room_1 = PianoRoom.objects.create(
+            room_num='F2-202',
+            piano_type='电子琴',
+            brand='A',
+            price_0=10,
+            price_1=8,
+            price_2=6,
+            usable=1,
+            art_ensemble=0
+        )
+        cls.temp_room_2 = PianoRoom.objects.create(
+            room_num='F2-203',
+            piano_type='钢琴',
+            brand='B',
+            price_0=10,
+            price_1=8,
+            price_2=6,
+            usable=1,
+            art_ensemble=0
+        )
+        cls.temp_room_3 = PianoRoom.objects.create(
+            room_num='F2-204',
+            piano_type='钢琴',
+            brand='B',
+            price_0=10,
+            price_1=8,
+            price_2=6,
+            usable=1,
+            art_ensemble=0
+        )
+        cls.query_dict_1 = {'room_num': 'F2-202', 'piano_type': '电子琴'}
+        cls.query_dict_2 = {'room_num': 'F2-202', 'piano_type': '钢琴'}
+        cls.query_dict_3 = {'piano_type': '钢琴'}
+        cls.query_dict_4 = {'piano_type': '小提琴'}
+        cls.query_dict_5 = {'room_num': 'F2-204'}
+        cls.query_dict_6 = {}
+        cls.answer_1 = {'room_list': {'A': [{'brand': 'A', 'room_num': 'F2-202', 'piano_type': '电子琴', 'price_0': 10,
+                                             'price_1': 8, 'price_2': 6, 'usable': True, 'art_ensemble': 0}]}}
+        cls.answer_2 = {'room_list': {}}
+        cls.answer_3 = {'room_list': {'B': [{'brand': 'B', 'room_num': 'F2-203', 'piano_type': '钢琴', 'price_0': 10,
+                                             'price_1': 8, 'price_2': 6, 'usable': True, 'art_ensemble': 0},
+                                            {'brand': 'B', 'room_num': 'F2-204', 'piano_type': '钢琴', 'price_0': 10,
+                                             'price_1': 8, 'price_2': 6, 'usable': True, 'art_ensemble': 0}]}}
+        cls.answer_4 = {'room_list': {}}
+        cls.answer_5 = {'room_list': {'B': [{'brand': 'B', 'room_num': 'F2-204', 'piano_type': '钢琴', 'price_0': 10,
+                                             'price_1': 8, 'price_2': 6, 'usable': True, 'art_ensemble': 0}]}}
+        cls.answer_6 = None
+
+    def test_post(self):
+        response = self.login_client.post('/a/piano-room/list/', self.query_dict_1)
+        self.assertEqual(response.json()['code'], 1)
+        self.assertEqual(response.json()['data'], self.answer_1)
+        response = self.login_client.post('/a/piano-room/list/', self.query_dict_2)
+        self.assertEqual(response.json()['code'], 1)
+        self.assertEqual(response.json()['data'], self.answer_2)
+        response = self.login_client.post('/a/piano-room/list/', self.query_dict_3)
+        self.assertEqual(response.json()['code'], 1)
+        self.assertEqual(response.json()['data'], self.answer_3)
+        response = self.login_client.post('/a/piano-room/list/', self.query_dict_4)
+        self.assertEqual(response.json()['code'], 1)
+        self.assertEqual(response.json()['data'], self.answer_4)
+        response = self.login_client.post('/a/piano-room/list/', self.query_dict_5)
+        self.assertEqual(response.json()['code'], 1)
+        self.assertEqual(response.json()['data'], self.answer_5)
+        response = self.login_client.post('/a/piano-room/list/', self.query_dict_6)
+        self.assertEqual(response.json()['code'], 0)
+        self.assertEqual(response.json()['msg'], 'fail to list piano room as no such piano type exist')
+        self.assertEqual(response.json()['data'], self.answer_6)
 
 
 class OrderListTest(MyTest):
