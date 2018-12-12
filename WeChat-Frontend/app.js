7f20b5f63171183ca9d88098453f1891a912e9be
Imalne <2478604502@qq.com>
2018-12-12 23:55:07 +0800

    卡片阴影调整，添加预约界面下拉刷新、左右手势切换日期、网络请求过慢过渡图标

--- WeChat-Frontend/app.js
+++ WeChat-Frontend/app.js
@@ -3,16 +3,16 @@ var util = require("utils/util.js")
 //app.js
 let app = App({
   onLaunch: function () {
-    try{
+    try {
       this.globalData.user_session = wx.getStorageSync("user_session");
     }
-    catch(e){
+    catch (e) {
       console.log("wrong when get local storage");
     }
-    if(!this.globalData.user_session){
-      this.getUserSession(()=>{});
+    if (!this.globalData.user_session) {
+      this.getUserSession(() => { });
     }
-    else{
+    else {
       console.log("in storage");
       console.log(this.globalData.user_session);
     }
@@ -39,7 +39,7 @@ let app = App({
       }
     })
   },
-  getUserSession(callback,arg1=null,arg2=null,arg3=null,arg4=null){
+  getUserSession(callback, arg1 = null, arg2 = null, arg3 = null, arg4 = null) {
     wx.login({
       success: res => {
         console.log("we.login return ");
@@ -54,13 +54,13 @@ let app = App({
             console.log("paino session return ");
             console.log(res);
             if (res.data["code"] == 1) {
-              this.globalData.user_session =  res.data["data"]["session"];
+              this.globalData.user_session = res.data["data"]["session"];
               wx.setStorageSync("user_session", this.globalData.user_session);
               console.log("get from net");
-              console .log(this.globalData.user_session);
-              callback(arg1,arg2,arg3,arg4);
+              console.log(this.globalData.user_session);
+              callback(arg1, arg2, arg3, arg4);
             }
-            else{
+            else {
               util.msgPrompt(res.data["msg"]);
             }
           },
@@ -74,27 +74,34 @@ let app = App({
       }
     })
   },
-  getRoomList(data,successFunc){
-      this._post('/u/order/piano-rooms-list/', data, successFunc)
+  getRoomList(data, successFunc) {
+    this._post('/u/order/piano-rooms-list/', data, successFunc)
   },
-  submitOrder(data,successFunc){
-    this._post(`/u/order/normal/`,data,successFunc)
+  submitOrder(data, successFunc) {
+    this._post(`/u/order/normal/`, data, successFunc)
   },
-  getOrderList(data,successFunc) {
-      this._get(`/u/order/list`,data,successFunc)
+  getOrderList(data, successFunc) {
+    this._get(`/u/order/list`, data, successFunc)
   },
-  changeOrder(data,successFunc){
-    this._post(`/u/order/change/`,data,successFunc)
+  changeOrder(data, successFunc) {
+    this._post(`/u/order/change/`, data, successFunc)
   },
-  cancelOrder(data,successFunc){
-    this._post(`/u/order/cancel/`,data,successFunc)
+  cancelOrder(data, successFunc) {
+    this._post(`/u/order/cancel/`, data, successFunc)
   },
-  _post(url,data,successFunc=res=>{console.log(res)},failFunc=res=>{console.log(res)}){
-    if(this.globalData.user_session)
-    {
-      console.log(url+"   data:");
+  _post(url, data, successFunc = res => { console.log(res) }, failFunc = res => { console.log(res) }) {
+    if (this.globalData.user_session) {
+      let _this = this;
+      this.globalData.network_busy = true;
+      setTimeout(() => {
+        if (_this.globalData.network_busy)
+          wx.showLoading({
+            title: 'loading',
+          })
+      }, this.globalData.network_waiting)
+      console.log(url + "   data:");
       console.log(data);
-      console.log("session: "+ this.globalData.user_session)
+      console.log("session: " + this.globalData.user_session)
       wx.request({
         url: this.globalData.backend + url,
         data: data,
@@ -103,25 +110,36 @@ let app = App({
         },
         method: "POST",
         success: res => {
-          console.log(url+"   return:");
+          console.log(url + "   return:");
           console.log(res);
-          if(res.data["code"] == 0)
+          if (res.data["code"] == 0)
             util.msgPrompt(res.data["msg"]);
           else
             successFunc(res);
         },
         fail: res => {
           failFunc(res);
+        },
+        complete: res => {
+          this.globalData.network_busy = false;
+          wx.hideLoading()
         }
       })
     }
-    else{
-      this.getUserSession(this._post,url,data,successFunc,failFunc);
+    else {
+      this.getUserSession(this._post, url, data, successFunc, failFunc);
     }
   },
   _get(url, data, successFunc = res => { console.log(res) }, failFunc = res => { console.log(res) }) {
-    if(this.globalData.user_session)
-    {  
+    if (this.globalData.user_session) {
+      let _this = this;
+      this.globalData.network_busy = true;
+      setTimeout(() => {
+        if (_this.globalData.network_busy)
+          wx.showLoading({
+            title: 'loading',
+          })
+      }, this.globalData.network_waiting)
       console.log(url + "   data:");
       console.log(data);
       console.log("session: " + this.globalData.user_session)
@@ -142,20 +160,24 @@ let app = App({
         },
         fail: res => {
           failFunc(res);
+        },
+        complete: res => {
+          this.globalData.network_busy = false;
+          wx.hideLoading()
         }
       })
     }
     else
-      this.getUserSession(this._get,url,data,successFunc,failFunc)
+      this.getUserSession(this._get, url, data, successFunc, failFunc)
   },
   globalData: {
     userInfo: null,
-    user_session:null,
-    user_nickname:"默认用户",
-    backend:`https://711602.iterator-traits.com`,
+    user_session: null,
+    user_nickname: "默认用户",
+    backend: `https://711602.iterator-traits.com`,
     // backend: `http://127.0.0.1:80`,
     // backend:`http://f23e7fdc.ngrok.io`,
-    user_session:null
+    user_session: null
   }
 })
 
