f62c6afb7b7f7e831ed095f3d4d9f71357df4a1a
Venessa-wei <fzw_16@163.com>
2018-12-22 17:30:53 +0800

    Add feedback async test

--- Admin-Frontend/test/unit/specs/feedback.spec.js
+++ Admin-Frontend/test/unit/specs/feedback.spec.js
@@ -13,31 +13,28 @@ Vue.use(ElementUI)
 window.test = true;
 
 describe('反馈信息列表界面', () => {
-    const vm = new Vue(App).$mount()
+  const vm = new Vue(App).$mount()
 
-    it('检查内容为空时页面元素', () => {
-        expect(vm.$el.querySelectorAll(".side-bar").length > 0).to.be.equal(true);
-        expect(vm.$el.querySelectorAll(".select-panel").length > 0).to.be.equal(true);
-        expect(vm.$el.querySelectorAll(".pagaination-div").length === 0).to.be.equal(true);
-        expect(vm.$el.querySelectorAll(".pagination").length > 0).to.be.equal(true);
-        expect(vm.$el.querySelectorAll(".feedback-content").length === 0).to.be.equal(true);
-        });
-    it('检查信息列表显示', done => {
-        vm.feedback_detail = {
-          feedback_list: [{ feedback_content: "test_content", feedback_time: 1544369401, read_status: false },
-          { feedback_content: "test_content", feedback_time: 1544369401, read_status: true }],
-          read_feedback_list: [{ feedback_content: "test_content", feedback_time: 1544369401, read_status: true }],
-          unread_feedback_list: [{ feedback_content: "test_content", feedback_time: 1544369401, read_status: false }] };;
-        vm.page_end = 1;
-        vm.total_len = 2;
-        console.log("hhhhhhhhhhhhhhhh")
-        Vue.nextTick(() => {
-          console.log("hhhhhhhhhhhhhhhh")
-          console.log(vm.$el.querySelectorAll(".feedback-content").length)
-          // expect(vm.$el.querySelectorAll(".pagaination-div").length > 0).to.be.equal(true);
-          expect(vm.$el.querySelectorAll(".feedback-content").length === 0).to.be.equal(true);
-          // expect(vm.$el.querySelectorAll("#pane-0 .el-table__row .el-table_1_column_3 .feedback-content")[0].text).to.be.equal("1test_content");
-          done()
-        })
+  it('检查内容为空时页面元素', () => {
+    expect(vm.$el.querySelectorAll(".side-bar").length > 0).to.be.equal(true);
+    expect(vm.$el.querySelectorAll(".select-panel").length > 0).to.be.equal(true);
+    expect(vm.$el.querySelectorAll(".pagaination-div").length === 0).to.be.equal(true);
+    expect(vm.$el.querySelectorAll(".pagination").length > 0).to.be.equal(true);
+    expect(vm.$el.querySelectorAll(".feedback-content").length === 0).to.be.equal(true);
+  });
+  it('检查信息列表显示', done => {
+    vm.feedback_list = [{ feedback_content: "test_content", feedback_time: 1544369401, read_status: false },
+        { feedback_content: "test_content", feedback_time: 1544369401, read_status: true }]
+      // ,
+      // read_feedback_list: [{ feedback_content: "test_content", feedback_time: 1544369401, read_status: true }],
+      // unread_feedback_list: [{ feedback_content: "test_content", feedback_time: 1544369401, read_status: false }] };;
+    vm.total_len = 2;
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelectorAll(".pagination-div").length > 0).to.be.equal(true);
+      expect(vm.$el.querySelectorAll(".feedback-content").length > 0).to.be.equal(true);
+      expect(vm.$el.querySelectorAll("#pane-0 .el-table__row .el-table_1_column_3 .feedback-content")[0].textContent).to.be.equal("test_content");
+      expect(vm.$el.querySelectorAll("#pane-0 .el-table__row .el-table_1_column_4 p")[0].textContent).to.be.equal('2018年12月9日 GMT+8下午11:30:01');
+      done()
     })
+  })
 })
