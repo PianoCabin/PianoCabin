cde9f5fd0c0170808ba1a8efa8b864f0e5211430
linlinhaohao888 <linlinhaohao.888@163.com>
2018-12-27 17:25:09 +0800

    -增加form_id

--- Backend/admin_end/models.py
+++ Backend/admin_end/models.py
@@ -54,6 +54,7 @@ class Order(models.Model):
     price = models.IntegerField(default=0)
     order_status = models.IntegerField(default=1)
     cancel_reason = models.IntegerField(default=0)
+    form_id = models.TextField(default='')
 
     def __str__(self):
         return self.id
